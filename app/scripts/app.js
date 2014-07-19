5f3672225c386bd049efc7dc97a959a0c584f865
Saba Samakar <saba.samakar@gmail.com>
2014-07-18 22:11:10 -0600

    Remove mongodb ref, authenticate user and add to scope.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,27 +1,20 @@
 'use strict';
 
-var mongo = require('mongodb').MongoClient;
-
 var app = angular.module('appointmentMakerApp', ['ui.router', 'ui.bootstrap', 'firebase']);
 
-var chatRef = new Firebase('https://flickering-fire-9095.firebaseio.com');
+var ref = new Firebase('https://flickering-fire-9095.firebaseio.com');
 
-// var express = require('express');
-// console.log(express);
-
-// var auth = new FirebaseSimpleLogin(chatRef, function(error, user, usersArray) {
-//   if (error) {
-//     // an error occurred while attempting login
-//     console.log(error);
-//   } else if (user) {
-//     // user authenticated with Firebase
-//     console.log('Name: ' + user.displayName + ', User ID: ' + user.uid + ', Provider: ' + user.provider);
-//     console.log(user.displayName);
-//   } else {
-//     // user is logged out
-//   }
-// });
-
+var auth = new FirebaseSimpleLogin(ref, function(error, user) {
+  if (error) {
+    // an error occurred while attempting login
+    console.log(error);
+  } else if (user) {
+    // user authenticated with Firebase
+    console.log('Name: ' + user.displayName + ', User ID: ' + user.uid + ', Provider: ' + user.provider);
+  } else {
+    // user is logged out
+  }
+});
 
 app.config(function($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise('/');
--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -1,14 +1,25 @@
 'use strict';
 
 angular.module('appointmentMakerApp')
-  .controller('HeaderCtrl', function ($scope, $location) {
+  .controller('HeaderCtrl', function ($scope, $rootScope, $location) {
 
   	$scope.isActive = function(viewLocation){
   		return viewLocation === $location.path();
   	}
 
+    $scope.otherThing = 'sfsfffsfsfsfsfsf'
+
   	$scope.loginBox = function(){
-  		return auth.login('google');
+      console.log("logging in")
+  		auth.login("google")
+        .then(function(user){
+          console.log(user)
+          $scope.$apply(function(){
+          $scope.user = user;
+          $scope.thing = 'sfsfsf'
+          $rootScope.user = user;
+          })
+        })
   	}
 
   	$scope.logout = function(){
